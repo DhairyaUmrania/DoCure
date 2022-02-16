f45739f5af34eb44e3b1f8ba09db1f166c272ba7
Tirth11 <59504521+Tirth11@users.noreply.github.com>
2022-02-16 17:35:42 +0530

    My project

---
+++ Auth/home/urls.py
@@ -0,0 +1,20 @@
+from django.urls import include, path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),
+	path('home/', views.home, name="home"),
+	path('logout/',views.logout_view,name='logout'),
+ 	path('about/',views.about,name='about'),
+ 	path('FILE/',views.FILE,name='FILE'),
+  path('homebefore/', views.homebefore, name="homebefore"),
+   
+
+
+	
+    ]
